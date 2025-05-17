7fe3d2b65fe2c74e9f5a7b9caf42df8d97312399
Isaac McFadyen <isaac@imcf.me>
2025-05-17 10:58:02 -0400

    feat: proper CLI parsing, OS exclusion

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,49 @@ use std::{
 	time::{Duration, Instant},
 };
 
-use eyre::OptionExt;
+use clap::{ArgAction, Parser};
+use eyre::{OptionExt, bail};
+use serde::Deserialize;
 use serde_json::Value;
 use tracing::Level;
 
-fn get_peers() -> eyre::Result<Vec<String>> {
+#[derive(Parser)]
+struct Args {
+	/// How often to ping peers in milliseconds.
+	#[clap(long, short, default_value = "5000")]
+	interval: u64,
+
+	/// Whether to exclude mobile devices (iOS, Android). Disabling this may increase battery usage!
+	#[clap(
+		long,
+		default_missing_value("true"),
+        default_value("true"),
+        num_args(0..=1),
+        require_equals(true),
+        action = ArgAction::Set,
+	)]
+	exclude_mobile: bool,
+
+	/// A comma-delimited list of devices to include. Mutually exclusive with the exclude argument.
+	#[clap(long, group = "filter")]
+	include: Option<String>,
+
+	/// A comma-delimited list of devices to exclude. Mutually exclusive with the include argument.
+	#[clap(long, group = "filter")]
+	exclude: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Peer {
+	#[serde(rename = "HostName")]
+	hostname: String,
+	#[serde(rename = "DNSName")]
+	dns_name: String,
+	#[serde(rename = "OS")]
+	os: String,
+}
+
+fn get_peers() -> eyre::Result<Vec<Peer>> {
 	let res = Command::new("tailscale")
 		.arg("status")
 		.arg("--json")
@@ -16,68 +54,72 @@ fn get_peers() -> eyre::Result<Vec<String>> {
 	let parsed: Value = serde_json::from_slice(&res.stdout)?;
 	let peer = parsed["Peer"]
 		.as_object()
-		.ok_or_eyre("Missing Peer entry from tailscale status!")?;
-	let ips = peer
-		.values()
-		.map(|v| v["DNSName"].as_str().map(|v| v.to_string()))
-		.collect::<Option<_>>()
-		.ok_or_eyre("Missing DNSName from entries in tailscale status!")?;
-	Ok(ips)
+		.ok_or_eyre("Missing Peer entry from tailscale status!")?
+		.clone();
+	let peers = peer
+		.into_values()
+		.map(serde_json::from_value)
+		.collect::<Result<Vec<Peer>, _>>()?;
+	Ok(peers)
 }
-fn ping_peer(addr: &str) -> eyre::Result<()> {
+fn ping_peer(peer: &Peer) -> eyre::Result<()> {
 	Command::new("ping")
 		.args(["-c", "1", "-i", "0.5"])
-		.arg(addr)
+		.arg(&peer.dns_name)
 		.stderr(Stdio::null())
 		.stdout(Stdio::null())
 		.spawn()?
 		.wait()?;
 	Ok(())
 }
-fn should_ping_peer(peer: &str, filter: &[&str]) -> bool {
-	for p in filter {
-		if peer.starts_with(p) {
-			return true;
-		}
+fn filter_peers(mut peers: Vec<Peer>, args: &Args) -> eyre::Result<Vec<Peer>> {
+	// First remove Android or IOS based on arg.
+	if args.exclude_mobile {
+		peers.retain(|v| v.os.to_lowercase() != "ios" && v.os.to_lowercase() != "android");
 	}
-	false
+
+	// Bail if both include and exclude are specified (shouldn't be possible b/c of clap group).
+	if args.include.is_some() && args.exclude.is_some() {
+		bail!("Include and exclude cannot be specified at the same time!")
+	}
+
+	if let Some(inc) = &args.include {
+		let inc = inc.split(",").map(|v| v.to_string()).collect::<Vec<_>>();
+		peers.retain(|v| inc.contains(&v.hostname));
+	} else if let Some(exc) = &args.exclude {
+		let exc = exc.split(",").map(|v| v.to_string()).collect::<Vec<_>>();
+		peers.retain(|v| !exc.contains(&v.hostname));
+	}
+
+	Ok(peers)
 }
 
 fn main() -> eyre::Result<()> {
 	tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-	let ping_time = std::env::var("PING_TIME_MS")
-		.unwrap_or("4000".to_string())
-		.parse::<u64>()?;
-	let peer_filter = std::env::var("PEER_FILTER").unwrap_or("".to_string());
-	let peer_filter = peer_filter.split(",").collect::<Vec<_>>();
+	let args = Args::parse();
 
-	tracing::info!("Pinger starting.");
-	tracing::info!("Ping time set to {}ms.", ping_time);
-	if peer_filter.is_empty() {
-		tracing::info!("No peer filter set, pinging all peers.");
-	} else {
-		tracing::info!("Using peer filter: {:?}", peer_filter);
-	}
+	tracing::info!("ts-pinger starting.");
+	tracing::info!("Ping interval set to {}ms.", args.interval);
 
 	let mut last_ping: Instant;
 	loop {
 		last_ping = Instant::now();
 
-		// Ping all peers.
+		// Pull peers and filter. Must do this here because peers may get added/removed.
 		let peers = get_peers()?;
+		let peers = filter_peers(peers, &args)?;
+
+		// Ping all peers.
 		tracing::info!("Ping starting:");
 		for peer in peers {
-			if !should_ping_peer(&peer, &peer_filter) {
-				continue;
-			}
-			tracing::info!("  Pinging {}", peer);
+			tracing::info!("  Pinging {}", peer.hostname);
 			ping_peer(&peer)?;
 		}
 
 		let since_last = last_ping.elapsed().as_millis() as u64;
-		if since_last < ping_time {
-			let to_wait = ping_time - since_last;
+		if since_last < args.interval {
+			let to_wait = args.interval - since_last;
 			tracing::info!("Waiting for {}ms before next ping.", to_wait);
 			std::thread::sleep(Duration::from_millis(to_wait));
 		}
